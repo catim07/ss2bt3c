348dcf3f3a86d113007ddb147b950654223638b8
catim07 <cuongdfle@gmail.com>
2024-12-09 22:03:09 +0700

    Add files via upload

---
+++ ss2bt3.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int a= 3107;
+	int b=2006;
+	int total=a+b;
+	int hieu=a-b;
+	int thuong=a/b;
+	int tich=a*b;
+	printf("tong: %d, hieu: %d, thuong: %d, tich: %d",total, hieu, thuong, tich);
+	return 0;
+}
